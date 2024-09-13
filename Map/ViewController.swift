a3267cb630c41e6a63b1e741906e88db8df4d3dc
paulomagro <114029366+paulomagro@users.noreply.github.com>
2024-09-13 20:56:58 -0300

    alterações para video1

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -2,18 +2,64 @@
 //  ViewController.swift
 //  Map
 //
-//  Created by Paulo Magro on 13/09/24.
+//  Created by Paulo Magro on 23/08/24.
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    let mapView: MKMapView = {
+        let mapView = MKMapView(frame: .zero)
+        mapView.showsUserLocation = false
+        mapView.showsUserTrackingButton = true
+        
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupView()
+        locationManager.delegate = self
     }
 
+    private func setupView() {
+        view.addSubview(mapView)
+        
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+}
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            locationManager.requestLocation()
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coordinate = locations.last?.coordinate {
+            let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 250, longitudinalMeters: 250)
+            mapView.region = region
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
 
